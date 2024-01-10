1638442a0a7ffef559a758052a2d3ca5cc5680f8
KaraJH <karas.jones@yahoo.com>
2024-01-10 11:24:22 -0700

    test: Expenses

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -27,4 +27,13 @@ RSpec.describe Department do
       expect(@customer_service.employees).to eq([@bobbi, @aaron])
     end
   end
+
+  describe "expense" do 
+    it "adds expenses to department" do 
+      @customer_service.expense(100)
+      @customer_service.expense(25)
+
+      expect(@customer_service.expenses).to eq(125)
+    end
+  end
 end
