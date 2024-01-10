d063596ad97496f26a43c8b17114ae4c42fa0bce
KaraJH <karas.jones@yahoo.com>
2024-01-10 11:02:21 -0700

    test: Set up test for employee attributes

---
+++ spec/employee_spec.rb
@@ -0,0 +1,14 @@
+require './lib/employee'
+require 'rspec'
+
+RSpec.describe Employee do 
+  before(:each) do 
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+  end
+
+  describe "initialize" do 
+    expect(@bobbi).to be_a(Employee)
+    expect(@bobbi.name).to eq("Bobbi Jaeger")
+    expect(@bobbi.age).to eq(30)
+  end
+end
