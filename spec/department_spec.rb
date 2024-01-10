cf6ee7f29ac35b3df3fecec68a22e4377d1a75da
KaraJH <karas.jones@yahoo.com>
2024-01-10 11:20:49 -0700

    test: Move employee attribute to initialize test block

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -14,11 +14,6 @@ RSpec.describe Department do
     it "exists and has a name" do 
       expect(@customer_service).to be_a(Department)
       expect(@customer_service.name).to eq("Customer Service")
-    end
-  end
-
-  describe "employees" do 
-    it "has a list of employees" do 
       expect(@customer_service.employees).to eq([])
     end
   end
